eb97167203dd8041e36559f72885c30724312392
haophamanh <hao.pham@hotschedules.com>
2019-01-08 18:21:00 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
 cat changecode.txt
-echo ${env.JOB_NAME}
 echo ${JOB_NAME}
 if [ "$JOB_NAME" = "test.downstream" ];
 then
-echo "test.downstream"
+echo "job test.downstream"
 else
 echo="job khac"
 fi
